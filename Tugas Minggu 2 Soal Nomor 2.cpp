258583966dc4eebe9870fb2d7eba82130a1a4dc4
raka <146154060+Raka444@users.noreply.github.com>
2023-09-27 10:30:04 +0700

    Add files via upload

---
+++ Tugas Minggu 2 Soal Nomor 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int tugas = 90;
+  int uts = 80;
+  int uas = 95;
+  float averange = tugas+uts+uas;
+  float nilaiakhir=averange/3;
+  cout << nilaiakhir;
+    
+}
+ 
+
+
+
+
+
+  
+    
